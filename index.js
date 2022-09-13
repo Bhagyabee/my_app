ebd89b0c7ba09e50be88678ec9204f76af758392
Bhagyabee <bhagyabeeb@email.com>
2022-09-13 16:18:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+ const express=require('express');
+ const app=express();
+ const port=8000;
+
+
+
+
+
+
+
+
+ // telling the server to listen on port
+ app.listen(port,(err)=>{
+    if(err){
+        console.log('error');
+    }
+    console.log('server is running on port',port);
+ })
