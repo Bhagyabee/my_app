dee9b82de11e130eb7066b2ff882f17a1558cabc
Bhagyabee <bhagyabeeb@email.com>
2022-09-17 12:38:21 +0530

    created user signin and signup

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+// const User=require('../models/users');
+
+
 module.exports.profile=function(req,res){
     return res.end("<h1> Profile page of user</h1>");
 }
@@ -5,21 +8,41 @@ module.exports.profile=function(req,res){
 //render sign-up page
 
 module.exports.sign_up=function(req,res){
- return res.render('user_signup',{
-    title: 'sign_up page'
- })
+ return res.render('user_signup', {
+   title: "signup"
+ });
 };
 
 
 // render sign in page
 module.exports.sign_in=function(req,res){
     return res.render('user_signin',{
-       title: 'sign_in page'
-    })
-   };
+      title: "signin"
+    });
+};
 
-module.exports.create=function(req,res){
-   //to do later
+ module.exports.create=function(req,res){
+   
+   if(req.body.password != req.body.confirm_password){
+      return res.redirect('/');
+   }
+  User.findOne({ email : req.body.email,function(err,user){
+     if(err){
+         console.log('error in creating user while signing up');return;
+      }
+       if(!user){
+         User.create(req.body,(err,user)=>{
+             if(err){
+                console.log('error in creating user while signing up');return;
+             }
+             return res.redirect('/users/sign-in');
+          })
+       }
+       else{
+          return res.redirect('/');
+       }
+    }})
+
 }
 module.exports.create_session=function(req,res){
    //to do later
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
  const express=require('express');
+ const cookieParser=require('cookie-parser');
+ 
  const app=express();
  const port=8000;
-const cookieParser=require('cookie-parser');
+
  //setting up database
  const db=require('./config/mongoose');
-
  
 app.use(express.urlencoded());
-app.use(cookieParser);
+app.use(cookieParser());
+
 
 
 
